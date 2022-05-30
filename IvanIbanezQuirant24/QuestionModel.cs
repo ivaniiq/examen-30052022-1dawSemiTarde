8ba526d46169236fb7efef1448898eb5f83aa783
Ivan Ibañez Quirant <gizboy@hotmail.com>
2022-05-30 21:16:33 +0200

    Creacion clase QuestionModel

---
+++ IvanIbanezQuirant24/QuestionModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IvanIbanezQuirant24
+{
+    internal class QuestionModel
+    {
+        private string question;
+        private string [] answer;
+        private int value;
+
+        public QuestionModel(string question, string[] answer, int value)
+        {
+            this.Question = question;
+            this.Answer = answer;
+            this.Value = value;
+        }
+
+        public string Question { get => question; set => question = value; }
+        public string[] Answer { get => answer; set => answer = value; }
+        public int Value { get => value; set => this.value = value; }
+    }
+}
